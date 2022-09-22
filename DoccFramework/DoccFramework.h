a4f7685494b67766c82536efe395e2a861dc6f12
Paul Peelen <ppeelen@paypal.com>
2022-09-22 15:41:18 +0200

    Initial Commit

---
+++ DoccFramework/DoccFramework.h
@@ -0,0 +1,18 @@
+//
+//  DoccFramework.h
+//  DoccFramework
+//
+//  Created by Paul Peelen on 2022-09-22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DoccFramework.
+FOUNDATION_EXPORT double DoccFrameworkVersionNumber;
+
+//! Project version string for DoccFramework.
+FOUNDATION_EXPORT const unsigned char DoccFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DoccFramework/PublicHeader.h>
+
+
